84291d2f55fe5e892631925f5e64c605a1cd7db1
martin0713 <martin.chiu@kkday.com>
2024-03-14 10:45:45 +0800

    feat: modify validation failed message

--- app/Http/Requests/ArticleUpdateRequest.php
+++ app/Http/Requests/ArticleUpdateRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ArticleUpdateRequest extends FormRequest
 {
@@ -31,5 +33,22 @@ class ArticleUpdateRequest extends FormRequest
         ];
     }
 
-    protected $redirect = '/failure/update';
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title field is required.',
+            'title.string' => 'The title must be a string.',
+            'body.required' => 'The body field is required.',
+            'body.string' => 'The body must be a string.',
+            'image.required' => 'The image field is required.',
+            'image.string' => 'The image must be a string.',
+            'tags.required' => 'The tags field is required.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        //write your bussiness logic here otherwise it will give same old JSON response
+        throw new HttpResponseException(response()->json($validator->errors(), 400));
+    }
 }
--- app/Http/Requests/UserLoginRequest.php
+++ app/Http/Requests/UserLoginRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UserLoginRequest extends FormRequest
 {
@@ -29,5 +31,19 @@ class UserLoginRequest extends FormRequest
         ];
     }
 
-    protected $redirect = '/api/auth/login';
+    public function messages()
+    {
+        return [
+            'email.required' => 'The email field is required.',
+            'email.email' => 'The email must be a valid email address.',
+            'password.required' => 'The password field is required.',
+            'password.string' => 'The password must be a string.',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        //write your bussiness logic here otherwise it will give same old JSON response
+        throw new HttpResponseException(response()->json($validator->errors(), 400));
+    }
 }
