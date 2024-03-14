5792f3743d2944df062a5968ef988f05574dfe4b
martin0713 <martin.chiu@kkday.com>
2024-03-14 17:52:11 +0800

    test: update article test

--- tests/Feature/ArticleControllerTest.php
+++ tests/Feature/ArticleControllerTest.php
@@ -2,77 +2,80 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Article;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ArticleControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
+    use RefreshDatabase;
 
     public function testStore()
     {
-        $user_id = rand(1,3);
-        $this->post(route('articles.store'), [
+        $user = User::factory()->create();
+        $this->actingAs($user)->post(route('articles.store'), [
             'title' => 'Test Article Title',
             'body' => 'Test Article Body',
-            'user_id' => $user_id,
+            'records' => ['time' => 1],
+            'user_id' => $user->id,
             'image' => 'Test Article Image',
         ]);
         $this->assertDatabaseHas('articles', [
             'title' => 'Test Article Title',
             'body' => 'Test Article Body',
-            'user_id' => $user_id,
+            'user_id' => $user->id,
             'image' => 'Test Article Image',
         ]);
     }
     public function testUpdate()
     {
+        $user = User::factory()->create();
         $article = new Article;
         $article->title = '';
         $article->body = '';
-        $article->records = array('time' => 1);
-        $article->user_id = rand(1,3);
+        $article->records = ['time' => 1];
+        $article->user_id = $user->id;
         $article->image = '';
         $article->save();
-
-        $this->put(route('articles.update', $article), [
+        $this->actingAs($user)->put(route('articles.update', [$article]), [
             'title' => 'Test Article Title Updated',
             'body' => 'Test Article Body Updated',
             'image' => 'Test Article Image Updated',
+            'tags' => [0]
         ]);
         $this->assertDatabaseHas('articles', [
             'title' => 'Test Article Title Updated',
             'body' => 'Test Article Body Updated',
             'image' => 'Test Article Image Updated',
         ]);
+        $this->assertDatabaseHas('article_tag', [
+            'article_id' => $article->id,
+            'tag_id' => 0,
+        ]);
     }
+
     public function testDestroy()
     {
+        $user = User::factory()->create();
         $article = new Article;
         $article->title = 'Test Article Title Deleted';
         $article->body = 'Test Article Body Deleted';
-        $article->records = array('time' => 1);
-        $article->user_id = rand(1,3);
+        $article->records = ['time' => 1];
+        $article->user_id = $user->id;
         $article->image = 'Test Article Image Deleted';
         $article->save();
+        $article->tags()->attach(1);
 
-        $this->delete(route('articles.destroy', $article));
+        $this->actingAs($user)->delete(route('articles.destroy', $article));
         $this->assertSoftDeleted('articles', [
             'title' => 'Test Article Title Deleted',
             'body' => 'Test Article Body Deleted',
             'image' => 'Test Article Image Deleted',
+        ]);
+        $this->assertDatabaseMissing('article_tag', [
+            'article_id' => $article->id,
+            'tag_id' => 1,
         ]);
     }
 }
