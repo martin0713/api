6a6adbe669d6d91d6b225cf76f37a4c73b887520
martin0713 <martin.chiu@kkday.com>
2024-03-15 15:44:00 +0800

    refactor: add redis repository

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,43 +3,45 @@
 namespace App\Services;
 
 use App\Repositories\UserRepository;
+use App\Repositories\RedisRepository;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Support\Facades\Redis;
 
 class UserService
 {
-    public function __construct(private readonly UserRepository $repo)
-    {
+
+    public function __construct(
+        private readonly UserRepository $userRepo,
+        private readonly RedisRepository $redisRepo
+    ) {
     }
 
     public function find(string $id): User |null
     {
-        $user = Redis::get("user:$id");
+        $user = $this->redisRepo->getUser($id);
         if ($user === null) {
-            $user = $this->repo->find($id);
+            $user = $this->userRepo->find($id);
             if ($user === null) {
                 throw new HttpResponseException(response()->json(['message' => 'User not found'], 404));
             }
-            Redis::set("user:$id", serialize($user));
+            $this->redisRepo->setUser($id, $user);
             return $user;
         }
-        $user = unserialize($user);
-        return $user;
+        return unserialize($user);
     }
 
     public function create(array $validated): \Illuminate\Http\JsonResponse |User
     {
-        $user = $this->repo->getUserByEmail($validated['email']);
+        $user = $this->userRepo->getUserByEmail($validated['email']);
         if (isset($user)) {
             throw new HttpResponseException(response()->json(['message' => 'User already exists'], 422));
         }
 
         $validated['password'] = password_hash($validated['password'], PASSWORD_BCRYPT);
-        $user = $this->repo->create($validated);
+        $user = $this->userRepo->create($validated);
         Auth::login($user);
-        Redis::set("user:$user->id", serialize($user));
+        $this->redisRepo->setUser($user->id, $user);
         return $user;
     }
 
@@ -47,7 +49,7 @@ class UserService
     {
         if (Auth::attempt(['email' => $validated['email'], 'password' => $validated['password']])) {
             $user = Auth::user();
-            Redis::set("user:$user->id", serialize($user));
+            $this->redisRepo->setUser($user->id, $user);
             return $user;
         } else {
             throw new HttpResponseException(response()->json(['message' => 'Invalid credentials'], 422));
@@ -64,11 +66,11 @@ class UserService
 
     public function update(array $validated, string $userId): \Illuminate\Http\JsonResponse |User
     {
-        $result = $this->repo->update($validated, $userId);
+        $result = $this->userRepo->update($validated, $userId);
         if ($result) {
             $user = Auth::user()->refresh();
             Auth::login($user);
-            Redis::set("user:$userId", serialize($user));
+            $this->redisRepo->setUser($userId, $user);
             return $user;
         }
         throw new HttpResponseException(response()->json(['message' => 'Fail to update']));
@@ -76,9 +78,9 @@ class UserService
 
     public function delete(string $userId): string
     {
-        $result = $this->repo->delete($userId);
+        $result = $this->userRepo->delete($userId);
         if ($result) {
-            Redis::del("user:$userId");
+            $this->redisRepo->deleteUser($userId);
             return true;
         }
         return false;
